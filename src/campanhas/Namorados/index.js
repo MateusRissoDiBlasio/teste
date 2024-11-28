ff2cfe637a87ae4a6dcca255eb457ba8c3b53768
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-11-28 09:32:35 -0300

    Add files via upload

---
+++ src/campanhas/Namorados/index.js
@@ -0,0 +1,21 @@
+function ruaOliveiraBotelho(){
+    document.getElementById('btn1').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn1', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox1').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522999235157"; }, 2200);
+    
+}
+
+function ruaMoisesAmelio(){
+    document.getElementById('btn2').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn2', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox2').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522996222125"; }, 2000);
+}
+
+function ruaGalianoDasNeves(){
+    document.getElementById('btn3').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn3', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox3').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522998815670"; }, 2000);
+}
