48bc0d0b78fb5bc23edc7147ae966c6aaf986435
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-11-28 11:32:46 -0300

    Update index.js

--- src/campanhas/Namorados/index.js
+++ src/campanhas/Namorados/index.js
@@ -1,8 +1,7 @@
 var perfEntries = performance.getEntriesByType("navigation");
 
 if (perfEntries[0].type === "back_forward") {
-    window.location.href="https://mateusrissodiblasio.github.io/teste/";
-    // location.reload();
+    window.location.href="https://mateusrissodiblasio.github.io/teste/src/campanhas/Namorados/namorados.html";
 }
 
 function ruaOliveiraBotelho(){
