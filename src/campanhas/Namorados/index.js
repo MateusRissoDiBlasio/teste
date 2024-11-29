d26f42b34ea723f9d89c1204d81403c8abed4854
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-11-29 11:17:29 -0300

    Update index.js

--- src/campanhas/Namorados/index.js
+++ src/campanhas/Namorados/index.js
@@ -3,9 +3,7 @@ let count = 0;
 const firstIntervalId = setInterval(() => {
     if(document.getElementById('checkbox1').checked === true){
     
-    setTimeout(function () {document.getElementById('checkbox2').checked = false;},2600);
-    setTimeout(function () {document.getElementById('btn2', 'msg', 'bag').classList.remove('visibillity');},2000);
-
+    setTimeout(function () {document.getElementById('checkbox2').checked = false; document.getElementById('btn2', 'msg', 'bag').classList.remove('visibillity');},2600);
     }{};
 
     if(document.getElementById('checkbox2').checked === true){
