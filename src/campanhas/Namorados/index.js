68187d3888298bc92a2c8442fad866f489e56d82
MATEUS RISSO DI BLASIO <139410293+MateusRissoDiBlasio@users.noreply.github.com>
2024-11-29 10:44:57 -0300

    Add files via upload

--- src/campanhas/Namorados/index.js
+++ src/campanhas/Namorados/index.js
@@ -1,3 +1,52 @@
+let count = 0;
+
+const firstIntervalId = setInterval(() => {
+    if(document.getElementById('checkbox1').checked === true){
+    
+    setTimeout(function () {document.getElementById('checkbox1').checked = false; document.getElementById('btn1', 'msg', 'bag').classList.remove('visibillity');},2600);
+
+    }{};
+
+    if(document.getElementById('checkbox2').checked === true){
+    
+    setTimeout(function () {document.getElementById('checkbox2').checked = false; document.getElementById('btn2', 'msg', 'bag').classList.remove('visibillity');},2600);
+    }{};
+
+    if(document.getElementById('checkbox3').checked === true){
+    
+    setTimeout(function () {document.getElementById('checkbox3').checked = false; document.getElementById('btn3', 'msg', 'bag').classList.remove('visibillity');},2600);
+    }{};
+    
+    count = count+1.5;
+    console.log(count);
+    console.log(document.getElementById('checkbox1').checked === true);
+    console.log(document.getElementById('checkbox2').checked === true);
+    console.log(document.getElementById('checkbox3').checked === true);
+}, 1500);
+
+function ruaOliveiraBotelho(){
+    document.getElementById('btn1').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn1', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox1').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522999235157"; }, 2000);  
+}
+
+function ruaMoisesAmelio(){
+    document.getElementById('btn2').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn2', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox2').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522996222125"; }, 2000);
+}
+
+function ruaGalianoDasNeves(){
+    document.getElementById('btn3').addEventListener('click', function() {
+        setTimeout(function () {document.getElementById('btn3', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox3').checked = true;}, 1000);
+    });
+    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522998815670"; }, 2000);
+}
+
+// TESTES PARA AJUSTES DA TRANSIÇÃO DO EFEITO
+
 // var perfEntries = performance.getEntriesByType("navigation");
 
 // if (perfEntries[0].type === "back_forward"){
@@ -7,9 +56,9 @@
 // document.getElementById('heart1').addEventListener('click', function() { document.getElementById('checkbox1').checked = false });
 // }
 // if (perfEntries[0].type == "reload"){
-    
+
 //     setTimeout(); 
-    
+
 // }{ location.reload(); }
 
 // var add = 0;
@@ -27,61 +76,12 @@
 
 // const intervalId = setInterval(() => {
 //   if(document.getElementById('checkbox1').checked === true){
-    
+
 //     setTimeout(function () {document.getElementById('checkbox1').checked = false;},3000);
 
 //   }{};
-  
+
 //   count = count+5;
 //   console.log(count); // This will log the number every second
 //   console.log(document.getElementById('checkbox1').checked === true)
 // }, 5000); // Executes every 1000 milliseconds (1 second)
-
-let count = 0;
-
-const firstIntervalId = setInterval(() => {
-    if(document.getElementById('checkbox1').checked === true){
-      
-      setTimeout(function () {document.getElementById('checkbox1').checked = false; document.getElementById('btn1', 'msg', 'bag').classList.remove('visibillity');},2600);
-  
-    }{};
-
-    if(document.getElementById('checkbox2').checked === true){
-      
-        setTimeout(function () {document.getElementById('checkbox2').checked = false; document.getElementById('btn2', 'msg', 'bag').classList.remove('visibillity');},2600);
-    }{};
-
-    if(document.getElementById('checkbox3').checked === true){
-    
-        setTimeout(function () {document.getElementById('checkbox3').checked = false; document.getElementById('btn3', 'msg', 'bag').classList.remove('visibillity');},2600);
-    }{};
-    count = count+1.5;
-    console.log(count);
-    console.log(document.getElementById('checkbox1').checked === true);
-    console.log(document.getElementById('checkbox2').checked === true);
-    console.log(document.getElementById('checkbox3').checked === true);
-  }, 1500);
-
-function ruaOliveiraBotelho(){
-    document.getElementById('btn1').addEventListener('click', function() {
-        setTimeout(function () {document.getElementById('btn1', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox1').checked = true;}, 1000);
-    });
-    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522999235157"; }, 2000);  
-    // setTimeout(function() { document.getElementById('heart1').addEventListener('click', function() {  document.getElementById('checkbox1').checked = false;
-
-    // });},2000);
-}
-
-function ruaMoisesAmelio(){
-    document.getElementById('btn2').addEventListener('click', function() {
-        setTimeout(function () {document.getElementById('btn2', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox2').checked = true;}, 1000);
-    });
-    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522996222125"; }, 2000);
-}
-
-function ruaGalianoDasNeves(){
-    document.getElementById('btn3').addEventListener('click', function() {
-        setTimeout(function () {document.getElementById('btn3', 'msg', 'bag').classList.add('visibillity'); document.getElementById('checkbox3').checked = true;}, 1000);
-    });
-    setTimeout(function () { window.location.href="https://api.whatsapp.com/send?phone=5522998815670"; }, 2000);
-}
